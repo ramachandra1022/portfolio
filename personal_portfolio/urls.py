40292d4ece96c2b6be56f07cefedec1c126d4ba7
RAMACHANDRA BABU N <40975710+ramachandra1022@users.noreply.github.com>
2022-09-19 16:16:57 +0530

    Add files via upload

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('hello_world.urls'))
     path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
+    staticfiles_urlpatterns(),
 ]

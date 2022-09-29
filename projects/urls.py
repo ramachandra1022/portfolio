4f8d19014a43615de30b99bdf7879cc138f74c20
RAMACHANDRA BABU N <40975710+ramachandra1022@users.noreply.github.com>
2022-09-29 15:47:15 +0530

    Add files via upload

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from django.conf import settings
 from . import views
 
 urlpatterns = [
     path("", views.project_index, name="project_index"),
     path("<int:pk>/", views.project_detail, name="project_detail"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
